641aae3f9a4839d120c7f2ba232c276a9f6c811f
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-09 12:42:39 +0530

    Add project files.

---
+++ ASSIGN02/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//2.Accept length and breadth of a rectangle and find perimeter.
+
+Console.WriteLine("enter the length and breadth of a rectangle");
+
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the length of a rectangle");
+
+int breadth = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the breadth of a rectangle");
+
+int P = 2 * (length * breadth);
+
+Console.WriteLine(P);
